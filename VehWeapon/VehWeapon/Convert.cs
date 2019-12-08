05d9031da945ffcb28eb34eaf3f933dc9da08449
SgtLuxer <luczkabuczka@gmail.com>
2019-12-08 19:53:46 +0100

    Add files

---
+++ VehWeapon/VehWeapon/Convert.cs
@@ -0,0 +1,21 @@
+﻿using RGiesecke.DllExport;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace VehWeapon
+{
+    public class DllEntry
+    {
+        [DllExport("_RVExtension@12", CallingConvention = System.Runtime.InteropServices.CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            outputSize--;
+
+            string WG_Input = function;
+            int WG_Input1 = WG_Input.IndexOf('_');
+            string WG_VehWeapon = WG_Input.Substring(0, WG_Input1);
+            output.Append(WG_VehWeapon);
+            return;
+        }
+    }
+}
